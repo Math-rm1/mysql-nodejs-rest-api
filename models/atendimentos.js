b5f5625ae5ba5f7909c47ef33fad46886a049c64
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-01-08 13:42:05 -0300

    adicionando data e retornando status

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,13 +1,19 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-  adiciona(atendimento) {
+  adiciona(atendimento, res) {
     const sql = 'INSERT INTO Atendimentos SET ?';
+    const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+    const data = moment(atendimento.data, 'DD-MM-YYYY').format(
+      'YYYY-MM-DD HH:mm:ss',
+    );
 
-    conexao.query(sql, atendimento, (erro, resultados) => {
-      if (erro) console.log(erro);
+    const atendimentoComData = { ...atendimento, dataCriacao, data };
+    conexao.query(sql, atendimentoComData, (erro, resultados) => {
+      if (erro) res.status(400).json(erro);
       else {
-        console.log(resultados);
+        res.status(201).json(resultados);
       }
     });
   }
