866688998d623f4ef5ec068724c2d93825b9e7c6
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-01-09 12:53:06 -0300

    config

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const conexao = require('./infraestrutura/conexao');
 const Tabelas = require('./infraestrutura/tabelas');
 const app = customExpress();
 
+const port = process.env.PORT || 3000;
+
 conexao.connect((erro) => {
   if (erro) {
     console.log(erro);
   } else {
     Tabelas.init(conexao);
     Tabelas.criarAtendimento();
-    app.listen(3000, () => {
+    app.listen(port, () => {
       console.log('Servidor rodando na porta 3000');
     });
   }
