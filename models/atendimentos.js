45d983b776b42e19a0d584486779209cd8b1ae33
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-01-09 10:55:21 -0300

    adicionando os verbos PATCH e DELETE

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -53,7 +53,7 @@ class Atendimento {
         if (erro) {
           res.status(400).json(erro);
         } else {
-          res.status(201).json(resultados);
+          res.status(201).json(atendimento);
         }
       });
     }
@@ -67,6 +67,36 @@ class Atendimento {
         res.status(400).json(erro);
       } else {
         res.status(200).json(atendimento);
+      }
+    });
+  }
+
+  altera(id, valores, res) {
+    if (valores.data) {
+      valores.data = moment(valores.data, 'DD-MM-YYYY').format(
+        'YYYY-MM-DD HH:mm:ss',
+      );
+    }
+
+    const sql = `UPDATE Atendimentos SET ? WHERE id = ?`;
+    conexao.query(sql, [valores, id], (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ ...valores, id: id });
+      }
+    });
+  }
+
+  deleta(id, res) {
+    const sql = `DELETE FROM Atendimentos WHERE id =${id}`;
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({
+          id: id,
+        });
       }
     });
   }
