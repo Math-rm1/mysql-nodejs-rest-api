eca5f6385272d02818427268427a00999324114f
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-01-09 13:18:32 -0300

    atualizando dados da conexao

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: 'root',
-  database: 'agenda-petshop',
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b86a6d3beb0e3c',
+  password: 'a6f5a083',
+  database: 'heroku_517f83d60128043',
 });
 
 module.exports = conexao;
